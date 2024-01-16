71d24fc239485f7d980bc53c52f3d9254d89f59c
ChenKua <kua.chen@mail.mcgill.ca>
2024-01-16 18:27:11 -0500

    init

---
+++ driver.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def run_another_python_script_with_args(script_path, arg1, arg2):
+    try:
+        subprocess.run(["python", script_path, arg1, arg2], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+
+# Example: Running "example_script_with_args.py" with arguments "value1" and "value2"
+script_path = "module.py"
+arg1 = "value1"
+arg2 = "value2"
+run_another_python_script_with_args(script_path, arg1, arg2)
